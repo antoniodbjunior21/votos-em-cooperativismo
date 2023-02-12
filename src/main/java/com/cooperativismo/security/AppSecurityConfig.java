e6c09e1b85747458b785d26e54206aef6acc8312
antonio.junior <antoniodbjunior21@gmail.com>
2023-02-12 15:02:50 -0400

    configurado abertura de filtros CORS

--- src/main/java/com/cooperativismo/security/AppSecurityConfig.java
+++ src/main/java/com/cooperativismo/security/AppSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.cooperativismo.security;
 
+import com.cooperativismo.utils.Constantes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +16,16 @@ public class AppSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.cors().and().authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().permitAll();
         return http.build();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(Constantes.API_V1_URL + "**").allowedOrigins("*");
+            }
+        };
     }
 }
